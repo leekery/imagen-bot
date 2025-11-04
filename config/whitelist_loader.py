c0a2de5fc7d40d50a6e939234f682acd884ed3af
leekery <pashokmoreplay@gmail.com>
2025-11-04 15:48:42 +0300

    fix: whitelist_loader.py

--- config/whitelist_loader.py
+++ config/whitelist_loader.py
@@ -4,6 +4,5 @@ from whitelist import Whitelist
 WL_PATH = "whitelist.json"
 whitelist = Whitelist.load(WL_PATH)
 
-# Примеры:
 # whitelist.is_allowed(message.from_user.id)
 # whitelist.add_user(6165946917); whitelist.save()
